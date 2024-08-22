a2ed5c6b7a76a9c36255cb131ef300f68534a406
DanPeled <98838880+DanPeled@users.noreply.github.com>
2024-08-22 12:19:00 +0300

    made the deploy command remove the base href line that makes ghpages not work

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-REPO_URL = $(git remote get-url origin)
+REPO_URL=$(git remote get-url origin)
 flutter build web --web-renderer html
 
 cd build/web
 
+sed -i '/<base href="\/">/d' index.html
+
 git init
 git add .
 git commit -m "GitHub Pages Deploy"
-git branch -m ghpages
+git branch -M ghpages
 git remote add origin $REPO_URL
 git push -u origin ghpages --force
