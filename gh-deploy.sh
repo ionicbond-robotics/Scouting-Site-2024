fcdbc3fdf09f2c80753fb5d8d9dfa557e5ef7cd4
DanPeled <98838880+DanPeled@users.noreply.github.com>
2024-08-22 12:12:13 +0300

    created ghpages deploy command

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -1,5 +1,11 @@
-flutter build --web-renderer html
+REPO_URL = $(git remote get-url origin)
+flutter build web --web-renderer html
+
 cd build/web
+
+git init
 git add .
 git commit -m "GitHub Pages Deploy"
-git push --force
+git branch -m ghpages
+git remote add origin $REPO_URL
+git push -u origin ghpages --force
